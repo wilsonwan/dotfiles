340d04c6cc2a41bc2fefb6d5bf645e12a385051d
wilsonwan <mail@wilsonwan.dev>
2026-04-30 15:04:33 +0100

    Refactor fnm and starship configurations for improved functionality and clarity

--- linux/fish/.config/fish/conf.d/fnm.fish
+++ linux/fish/.config/fish/conf.d/fnm.fish
@@ -7,5 +7,5 @@ end
 
 if [ -d "$FNM_PATH" ]
   set PATH "$FNM_PATH" $PATH
-  fnm env --shell fish | source
+  fnm env --use-on-cd --shell fish | source
 end
--- linux/fish/.config/fish/config.fish
+++ linux/fish/.config/fish/config.fish
@@ -1,7 +1,10 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 end
+
 export PATH="$HOME/.local/bin:$PATH"
 
 # startship
-starship init fish | source
+if command -q starship
+    source (starship init fish --print-full-init | psub)
+end
